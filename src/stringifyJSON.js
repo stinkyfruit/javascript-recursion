ca1ef78844d197054c4007cde2a163b8823cdba4
Kristen Haydel <kristen@whatever.com>
2015-10-22 20:42:14 -0500

    Reimplemented JSON.stringify using recursion

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,55 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+    // your code goes here
+    if (obj === null) {
+        return "null";
+    }
+
+    if (obj === undefined || obj.constructor === Function) {
+        return;
+    }
+
+    if (obj.constructor === String) {
+        return '"' + obj + '"';
+    }
+    if (obj.constructor === Array) {
+        if (obj.length) {
+            var JSONstring = [];
+            for (var i = 0; i < obj.length; i++) {
+                JSONstring.push(stringifyJSON(obj[i]));
+            }
+            return '[' + JSONstring.join(",") + ']';
+        } else {
+            return '[]';
+        }
+    }
+    if (obj.constructor === Object) {
+        var keys = Object.keys(obj);
+        if (keys.length) {
+            var JSONstring = '';
+
+            for (var i = 0; i < keys.length; i++) {
+                var key = keys[i];
+
+                if (!key || obj[key] === undefined || typeof key === 'function' || typeof obj[key] === 'function') {
+
+                } else {
+                    if (i === keys.length - 1) {
+                        JSONstring += stringifyJSON(key) + ':' + stringifyJSON(obj[key]); // recursion
+                    } else {
+                        JSONstring += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ','; // recursion
+                    }
+                }
+            }
+            return '{' + JSONstring + '}';
+        } else {
+            return '{}';
+        }
+    }
+
+    return obj.toString();
+
+
+
 };
